ce5a59d1f4f51ba5b2c596d66a8df90d3c74e70a
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-11-27 17:37:03 +0100

    fizzbuzz kata

---
+++ exercises/kata/fizzbuzz01/main.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let mapping: [(Int, String)] = [
+    (3, "fizz"),
+    (5, "buzz"),
+    (7, "clang"),
+]
+
+let args = CommandLine.arguments
+
+if args.count != 2 {
+    print("Invalid number of arguments passed.\nUsage: /bin *integer*")
+    exit(1)
+}
+
+guard let number = Int(CommandLine.arguments[1]) else {
+    print("Passed argument is not a valid integer. Exiting.")
+    exit(1)
+}
+
+for (key, value) in mapping {
+    if number % key == 0 {
+        print(value, terminator: " ")
+    }
+}
+
+print()
