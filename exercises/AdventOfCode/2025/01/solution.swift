0f03c9072fcc3daa8353311f2becb540b80a2218
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-12-01 17:45:09 +0100

    aoc [01 / 12] PART 2

--- exercises/AdventOfCode/2025/01/solution.swift
+++ exercises/AdventOfCode/2025/01/solution.swift
@@ -7,17 +7,36 @@ let input = try String(contentsOfFile: filePath, encoding: .utf8)
 let MAX = 100
 var position = 50
 var countZeros = 0
+var countZerosPassed = 0
+var lastSign = 1
 
 for line in input.split(whereSeparator: \.isNewline) {
     let direction = line.first
-    let number = Int(line.dropFirst())!
+    let step = Int(line.dropFirst())!
 
-    let sign = { if direction == "L" { return 1 } else { return -1 } }()
+    let sign = { if direction == "L" { return -1 } else { return 1 } }()
+    let shifted = sign * step + position
 
-    let shifted = sign * number + position
+    let x: Int
+
+    if direction == "R" {
+        x = (position + step) / MAX
+    } else {
+
+        switch position {
+        case 0:
+            x = step / MAX
+        default:
+            x = (100 - position + step) / MAX
+        }
+
+    }
+
+    countZerosPassed += x
+
     position = (shifted % MAX + MAX) % MAX
-
     if position == 0 { countZeros += 1 }
 }
 
-print(countZeros)
+print("Part1: ", countZeros)
+print("Part2: ", countZerosPassed)
