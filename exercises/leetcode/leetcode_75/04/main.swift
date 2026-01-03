ea50e6a0dfbc5c683fcb6a20b10e6051792d67e4
Maciej Szalewicz <m.szalewicz@gmail.com>
2026-01-03 10:08:07 +0100

    cleaner version for leetcode 75 04

--- exercises/leetcode/leetcode_75/04/main.swift
+++ exercises/leetcode/leetcode_75/04/main.swift
@@ -29,14 +29,6 @@ Constraints:
 
 class Solution {
     static func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
-        if flowerbed.count == 0 {
-            return false
-        }
-
-        if flowerbed.count == 1 && flowerbed[0] == 0 && n == 1 {
-            return true
-        }
-
         var plantable = 0
         var previousPlanted = Int.min
 
